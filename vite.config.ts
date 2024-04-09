82606cefaaab180b56ea6d53b06f54601b5e1f55
Yevhenii Danoi <geneforapp@gmail.com>
2024-04-09 11:33:04 +0300

    change vite config to use fullcalendar

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "./dist" },
+  build: { 
+    outDir: "./dist",  
+    rollupOptions: {
+      external: ["@fullcalendar/core"]
+    } },
 });
